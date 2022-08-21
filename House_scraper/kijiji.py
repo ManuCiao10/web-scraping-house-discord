ed526b87228d96cdc8e7dcc082b7c157529af254
ManuCiao10 <emanuele.ardinghi@gmail.com>
2022-08-21 18:25:26 -0400

    Loop Fixed

--- House_scraper/kijiji.py
+++ House_scraper/kijiji.py
@@ -6,6 +6,11 @@ from bs4 import BeautifulSoup
 from .constants import *
 import datetime
 import csv
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
 
 class Kijiji:
     def __init__(self):
@@ -22,52 +27,66 @@ class Kijiji:
 
     def scrape_data(self):
         while True:
-            for loop in self.soup.find_all("div", class_="clearfix"):
-                try:
-                    url = loop.find("a", class_="title").get("href")
-                    pid = url.split("/")[-1]
-                except AttributeError:
-                    continue
-                if pid not in self.pid_list:
+            try:
+                loops = self.soup.find_all("div", class_="clearfix")
+                for loop in loops:
                     try:
-                        address = (
-                            loop.find("a", class_="title")
-                            .text.rstrip()
-                            .lstrip()
-                            .capitalize()
-                        )
+                        url = loop.find("a", class_="title").get("href")
+                        pid = url.split("/")[-1]
+                        print("PID", pid)
                     except AttributeError:
+                        logger.debug(f"URL NOT FOUND\n")
                         continue
-                    try:
-                        price = loop.find("div", class_="price").text.split(",")[0]
-                    except AttributeError:
-                        continue
-                    try:
-                        local = (
-                            loop.find("div", class_="location")
-                            .find("span")
-                            .text.rstrip()
-                            .lstrip()
-                        )
-                    except AttributeError:
-                        continue
-                    try:
-                        img = (
-                            loop.find("div", class_="image").find("img").get("data-src")
-                        )
-                    except AttributeError:
-                        continue
-                    if img == None:
-                        continue
+
+                    if pid not in self.pid_list:
+                        try:
+                            address = (
+                                loop.find("a", class_="title")
+                                .text.rstrip()
+                                .lstrip()
+                                .capitalize()
+                            )
+                        except AttributeError:
+                            logger.debug(f"ADDRESS NOT FOUND\n")
+                            continue
+                        try:
+                            price = loop.find("div", class_="price").text.split(",")[0]
+                        except AttributeError:
+                            logger.debug(f"PRICE NOT FOUND\n")
+                            continue
+                        try:
+                            local = (
+                                loop.find("div", class_="location")
+                                .find("span")
+                                .text.rstrip()
+                                .lstrip()
+                            )
+                        except AttributeError:
+                            logger.debug(f"LOCAL NOT FOUND\n")
+                            continue
+                        try:
+                            img = (
+                                loop.find("div", class_="image")
+                                .find("img")
+                                .get("data-src")
+                            )
+                        except AttributeError:
+                            continue
+                        if img == None:
+                            continue
+                        else:
+                            data = [url, address, img, price, local]
+                        with open("data.csv", "a") as csvfile:
+                            writer = csv.writer(csvfile)
+                            writer.writerow([pid])
+                        self.pid_list.add(pid)
+                        send_webhook(data)
                     else:
-                        data = [url, address, img, price, local]
-                    with open("data.csv", "a") as csvfile:
-                        writer = csv.writer(csvfile)
-                        writer.writerow([pid])
-                    self.pid_list.add(pid)
-                    send_webhook(data)
-                else:
-                    self.payload()
+                        logger.debug(f"RUNNING ANOTHER REQUESTS\n")
+                        self.payload()
+            except:
+                logger.debug(f"LOOP ISSUES\n")
+                self.payload()
 
 
 def send_webhook(data):
@@ -78,7 +97,7 @@ def send_webhook(data):
     embed.set_thumbnail(url=data[2])
     embed.add_embed_field(name="• Area", value=data[4], inline=True)
     embed.add_embed_field(name="• Price/month", value=f"{data[3]}$", inline=True)
-    embed.set_footer(text="Kijiji", icon_url=KIJIJI_IMG)
+    embed.set_footer(text="Kijiji_Monitor", icon_url=KIJIJI_IMG)
     embed.set_timestamp()
     webhook.add_embed(embed)
     resp = webhook.execute()
