6d73451bf8ed2eb078cee9d46336a40517b3b1f4
ManuCiao10 <emanuele.ardinghi@gmail.com>
2022-08-25 22:57:04 -0400

    fixing with C

--- House_scraper/kijiji.py
+++ House_scraper/kijiji.py
@@ -8,7 +8,7 @@ import datetime
 import csv
 import logging
 from utils import *
-import psutil    
+
 
 # logger = logging.getLogger(__name__)
 # logging.basicConfig(level=logging.DEBUG)
@@ -18,7 +18,6 @@ class Kijiji:
     """Class for creating Kijiji Iterators"""
 
     def __init__(self):
-        print(psutil.process_iter())
         self.base_url = KIJIJI
         self.session = requests.Session()
         self.pid_list = set()
@@ -38,7 +37,7 @@ class Kijiji:
             return Kijiji.awaiting(self)
         else:
             return Kijiji.scrape_data(self, loops)
-    
+
     def scrape_data(self, loops):
         while True:
             for loop in loops:
@@ -122,6 +121,7 @@ class Kijiji:
             print(datetime.datetime.now().strftime("%H:%M:%S.%f"), "<|WEBHOOK SENT|>")
         time.sleep(2)
         return resp
+
 
 if __name__ == "__main__":
     Kijiji()
---
+++ House_scraper/running.c
@@ -0,0 +1,56 @@
+#include "unistd.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "sys/types.h"
+#include "sys/stat.h"
+#include "sys/wait.h"
+
+
+int call_python(char *argv[])
+{
+    pid_t pid;
+    int status;
+    int i;
+    char *argv2[2];
+    argv2[0] = "python3";
+    argv2[1] = argv[1];
+    pid = fork();
+    if (pid == 0)
+    {
+        execvp("python3", argv2);
+        printf("fuck thisé\n");
+        exit(0);
+    }
+    if (pid < 0)
+    {
+        perror("fork");
+        exit(1);
+    }
+    else
+    {
+        waitpid (pid, &status, -1);
+        return 0;
+    }
+
+    return (0);
+}
+
+
+void do_python(void)
+{
+
+    char *argv[2];
+    argv[0] = "python3";
+    argv[1] = "kijiji.py";
+    call_python(argv);
+    
+    argv[1] = "utils.py";
+    call_python(argv);
+
+}
+
+int main(void)
+{
+    do_python();
+}
